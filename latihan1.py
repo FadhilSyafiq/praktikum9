c85f96ef0903441d98f1cba52e8187f01d8e0802
FadhilSyafiq <syafiqabdullahfadhil@gmail.com>
2025-12-18 23:28:04 +0700

    menambahkan file

---
+++ latihan1.py
@@ -0,0 +1,15 @@
+txt = "Hello World"
+
+print("Jumlah karakter:", len(txt))
+
+print("Karakter terakhir:", txt[-1])
+
+print("Index 2 sampai 4:", txt[2:5])
+
+print("Tanpa spasi:", txt.replace(" ", ""))
+
+print("Huruf besar:", txt.upper())
+
+print("Huruf kecil:", txt.lower())
+
+print("Ganti H jadi J:", txt.replace("H", "J"))
